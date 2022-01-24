703f6277beb73246944090a4c09d9b93ce10ff68
Md Nahidul Islam <thenahidulbd@gmail.com>
2022-01-24 13:22:28 +0600

    fixed issues, removed related.js file

--- pages/api/post/index.js
+++ pages/api/post/index.js
@@ -1,10 +1,11 @@
 import client from "../../../utils/apollo/client";
-import { GET_POSTS } from "../../../utils/graphql/client/queries";
+import { GET_ALL_POSTS } from "../../../utils/graphql/client/queries";
+
 
 const getPosts = async (req, res) => {
 	try {
 		const { data } = await client.query({
-			query: GET_POSTS
+			query: GET_ALL_POSTS
 		});
 		res.status(200).json(data.posts);
 	} catch (error) {
